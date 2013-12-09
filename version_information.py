5f3f02171e9434e00f499d253d7d73d29d72942b
westurner <wes.turner@gmail.com>
2013-12-09 10:53:25 -0600

    Added check for pkg_resources version and extended __doc__

--- version_information.py
+++ version_information.py
@@ -1,6 +1,51 @@
 """
 An IPython extension that provides a magic command that displays
-a table with information about versions of dependency modules.
+a table with information about versions of installed modules.
+
+This makes it much easier to determine which versions of modules
+were installed in the source IPython interpreter's environment.
+
+Produces output in:
+
+* Plaintext (IPython [qt]console)
+* HTML (IPython notebook, ``nbconvert --to html``, ``--to slides``)
+* JSON (IPython notebook ``.ipynb`` files)
+* LaTeX (e.g. ``ipython nbconvert example.ipynb --to LaTeX --post PDF``)
+
+Usage
+======
+
+.. sourcecode:: ipython
+
+   In [1]: %load_ext version_information
+
+   In [2]: %version_information
+   Out[2]:
+   Software versions
+   Python 2.7.3 (default, Sep 26 2013, 20:08:41) [GCC 4.6.3]
+   IPython 2.0.0-dev
+   OS posix [linux2]
+
+   Mon Dec 09 10:21:40 2013 CST
+
+   In [3]: %version_information sphinx, jinja2
+   Out[3]:
+   Software versions
+   Python 2.7.3 (default, Sep 26 2013, 20:08:41) [GCC 4.6.3]
+   IPython 2.0.0-dev
+   OS posix [linux2]
+   sphinx 1.2b3
+   jinja2 2.7.1
+
+   Mon Dec 09 10:21:52 2013 CST
+
+.. note:: ``%version_information`` expects to find the module version in
+   ``<module>.__version__``.
+
+   If ``<module>.__version__`` is not set, it attempts to get a version
+   string with ``pkg_resources.require('<module>')[0].version``
+   (the ``version`` field from ``setup.py``).
+
 """
 import cgi
 import json
@@ -11,6 +56,10 @@ import time
 import IPython
 from IPython.core.magic import magics_class, line_magic, Magics
 
+try:
+    import pkg_resources
+except ImportError:
+    pkg_resources = None
 
 @magics_class
 class VersionInformation(Magics):
@@ -21,7 +70,7 @@ class VersionInformation(Magics):
 
         Usage:
 
-            %version_information [optional list of modules]
+            %version_information [optional comma-separated list of modules]
 
         """
         self.packages = [("Python", sys.version.replace("\n", "")),
@@ -38,7 +87,13 @@ class VersionInformation(Magics):
                     exec(compile(code, "<string>", "exec"), ns_g, ns_l)
                     self.packages.append((module, ns_l["version"]))
                 except Exception as e:
-                    self.packages.append((module, str(e)))
+                    try:
+                        if pkg_resources is None:
+                            raise
+                        version = pkg_resources.require(module)[0].version
+                        self.packages.append((module, version))
+                    except Exception as e:
+                        self.packages.append((module, str(e)))
 
         return self
 
