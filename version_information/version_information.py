2917ab9511db49de94c118ed6c20be58a2e26bd0
William J. Capehart <William.Capehart@sdsmt.edu>
2020-08-02 16:18:30 -0600

    Modification to _repr_html_() to accomodate use of a string type varialbe for html
    The _latex_escape was duplicated and used in place of the cgi.escape() object from previous versions of _repr_html_()

--- version_information/version_information.py
+++ version_information/version_information.py
@@ -120,12 +120,30 @@ class VersionInformation(Magics):
         else:
             return json.dumps(obj)
 
+    @staticmethod
+    def _htmltable_escape(str_):
+        CHARS = {
+            '&':  r'\&',
+            '%':  r'\%',
+            '$':  r'\$',
+            '#':  r'\#',
+            '_':  r'\_',
+            '{':  r'\letteropenbrace{}',
+            '}':  r'\letterclosebrace{}',
+            '~':  r'\lettertilde{}',
+            '^':  r'\letterhat{}',
+            '\\': r'\letterbackslash{}',
+            '>':  r'\textgreater',
+            '<':  r'\textless',
+        }
+        return u"".join([CHARS.get(c, c) for c in str_])
+
     def _repr_html_(self):
 
         html_table = "<table>"
         html_table += "<tr><th>Software</th><th>Version</th></tr>"
         for name, version in self.packages:
-            _version = html_table.escape(version)
+            _version = html_table._htmltable_escape(version)
             html_table += "<tr><td>%s</td><td>%s</td></tr>" % (name, _version)
 
         try:
